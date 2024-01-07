88ba4670225d88ac72f42350c8bd5a4fe6b68542
Alex <argeares97@gmail.com>
2024-01-07 15:46:33 +0200

    fix: rename local storage RPC field name

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -11,7 +11,7 @@ import { EnsDataItem } from '../web3/store/ensSlice';
 
 export enum LocalStorageKeys {
   EnsAddresses = 'EnsAddresses',
-  RpcUrls = 'RpcUrls',
+  RpcUrls = 'rpcUrls',
   RepresentingAddresses = 'representingAddresses',
   HelpModalVisible = 'isHelpModalVisible',
   GaslessVote = 'isGaslessVote',
