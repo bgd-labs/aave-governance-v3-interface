394e5807caf0a6167f1a8cf9e5c63586cd5c7960
Alex <argeares97@gmail.com>
2024-01-25 14:02:47 +0200

    fix: small button styles

--- src/ui/components/SmallButton.tsx
+++ src/ui/components/SmallButton.tsx
@@ -1,7 +1,169 @@
-import { Box, useTheme } from '@mui/system';
+import { Box, SxProps, useTheme } from '@mui/system';
 import React, { MouseEventHandler, ReactNode } from 'react';
 
+import { useStore } from '../../store';
 import { Spinner } from './Spinner';
+
+const borderSize = 3;
+
+interface BoxWith3DProps {
+  children: ReactNode;
+  contentColor?: string;
+  borderLinesColor?: string;
+  leftBorderColor?: string;
+  bottomBorderColor?: string;
+  wrapperCss?: SxProps;
+  css?: SxProps;
+  className?: string;
+  disabled?: boolean;
+}
+
+function InitialBox({ disabled, borderLinesColor, children }: BoxWith3DProps) {
+  const theme = useTheme();
+
+  return (
+    <Box
+      className="BoxWith3D__initial--box"
+      sx={{
+        position: 'relative',
+        pb: borderSize,
+        pl: borderSize,
+        hover: disabled
+          ? undefined
+          : {
+              '.BoxWith3D__left-shadow': {
+                transition: 'all 0.15s ease',
+                width: borderSize,
+                top: borderSize / 2,
+              },
+              '.BoxWith3D__bottom-shadow': {
+                transition: 'all 0.09s ease',
+                height: borderSize,
+                left: borderSize / 2,
+                width: `calc(100% + ${borderSize / 6}px)`,
+              },
+              '.BoxWith3D__content': {
+                transform: `translate(-0px, 0px)`,
+              },
+            },
+        '&:active': disabled
+          ? undefined
+          : {
+              '.BoxWith3D__left-shadow': {
+                transition: 'all 0.15s ease',
+                width: 0,
+                top: borderSize,
+              },
+              '.BoxWith3D__bottom-shadow': {
+                transition: 'all 0.09s ease',
+                height: 0,
+                left: -0.5,
+              },
+              '.BoxWith3D__content': {
+                transform: `translate(-${borderSize}px, ${borderSize}px)`,
+              },
+              '.SmallButton__inner': {
+                backgroundColor: `${theme.palette.$appBackground} !important`,
+                borderTop: `1px solid ${borderLinesColor}`,
+                borderRight: `1px solid ${borderLinesColor}`,
+              },
+            },
+      }}>
+      {children}
+    </Box>
+  );
+}
+
+function BoxWith3D({
+  disabled,
+  children,
+  wrapperCss,
+  contentColor = '$mainStable',
+  borderLinesColor = '$mainBorder',
+  leftBorderColor = '$middleLight',
+  bottomBorderColor = '$light',
+  css,
+  className,
+}: BoxWith3DProps) {
+  const { isRendered } = useStore();
+  const theme = useTheme();
+
+  const contentColorWithRender = isRendered
+    ? `${theme.palette[contentColor]} !important`
+    : theme.palette[contentColor];
+  const leftBorderColorWithRender = isRendered
+    ? `${theme.palette[leftBorderColor]} !important`
+    : theme.palette[leftBorderColor];
+  const bottomBorderColorWithRender = isRendered
+    ? `${theme.palette[bottomBorderColor]} !important`
+    : theme.palette[bottomBorderColor];
+  const borderLinesColorWithRender = isRendered
+    ? `${theme.palette[borderLinesColor]} !important`
+    : theme.palette[borderLinesColor];
+
+  return (
+    <Box sx={wrapperCss} className={`BoxWith3D ${className}`}>
+      <InitialBox
+        borderLinesColor={borderLinesColorWithRender}
+        disabled={disabled}>
+        <Box
+          className="BoxWith3D__left-shadow"
+          sx={{
+            position: 'absolute',
+            backgroundColor: leftBorderColorWithRender,
+            transform: 'skewY(-45deg)',
+            left: 0,
+            border: `1px solid ${borderLinesColorWithRender}`,
+            borderRight: 'unset !important',
+            transition: 'all 0.07s ease',
+            top: borderSize + 0.2,
+            height: `calc(100% - ${borderSize}px)`,
+            width: 0,
+          }}
+        />
+        <Box
+          className="BoxWith3D__bottom-shadow"
+          sx={{
+            position: 'absolute',
+            backgroundColor: bottomBorderColorWithRender,
+            transform: 'skewX(-45deg)',
+            bottom: 0,
+            border: `1px solid ${borderLinesColorWithRender}`,
+            borderTop: 'none !important',
+            borderLeft: 'none !important',
+            transition: 'all 0.07s ease',
+            width: `calc(100% - ${borderSize}px)`,
+            height: 0,
+            left: 0,
+          }}
+        />
+
+        <Box
+          className="BoxWith3D__content"
+          sx={{
+            position: 'relative',
+            zIndex: 3,
+            backgroundColor: contentColorWithRender,
+            transitionProperty: 'transform, width, height, background',
+            transitionDuration: '0.15s',
+            transitionTimingFunction: 'ease',
+            width: `calc(100% + ${borderSize}px)`,
+            transform: `translate(-${borderSize}px, ${borderSize}px)`,
+          }}>
+          <Box
+            sx={{
+              width: '100%',
+              height: '100%',
+              border: `1px solid ${borderLinesColorWithRender}`,
+              ...css,
+            }}>
+            {children}
+          </Box>
+        </Box>
+      </InitialBox>
+    </Box>
+  );
+}
 
 export interface SmallButtonProps {
   type?: 'button' | 'submit';
@@ -25,151 +187,68 @@ export function SmallButton({
       component="button"
       type={type}
       disabled={disabled || loading}
-      onClick={onClick}
-      sx={{
-        transition: 'all 0.2s ease',
-        border: `1px solid ${theme.palette.$main}`,
-        minWidth: 95,
-        height: 20,
-        position: 'relative',
-        hover: {
-          '.SmallButton__inner': {
-            borderTop: `1px solid ${theme.palette.$main}`,
-            borderRight: `1px solid ${theme.palette.$main}`,
-            width: 'calc(100% - 2px)',
-            height: 'calc(100% - 2px)',
-          },
-          '.SmallButton__rotateLine': {
-            backgroundColor: `${theme.palette.$main} !important`,
-          },
-        },
-        '&:active': {
-          '.SmallButton__inner': {
-            borderTop: `1px solid ${theme.palette.$main}`,
-            borderRight: `1px solid ${theme.palette.$main}`,
-            width: 'calc(100% - 4px)',
-            height: 'calc(100% - 4px)',
-            backgroundColor: theme.palette.$appBackground,
-          },
-          '.SmallButton__rotateLine': {
-            backgroundColor: '$mainBorder',
-          },
-        },
-        '&:disabled': {
-          cursor: 'not-allowed',
-          '.SmallButton__inner': {
-            cursor: 'not-allowed',
-            borderTop: `1px solid transparent`,
-            borderRight: `1px solid transparent`,
-            backgroundColor: '$disabled',
-            width: '100%',
-            height: '100%',
-          },
-        },
-        [theme.breakpoints.up('lg')]: {
-          minWidth: 102,
-          height: 22,
-        },
-      }}>
-      <Box
-        sx={{
-          width: '100%',
-          height: 4,
+      onClick={onClick}>
+      <BoxWith3D
+        disabled={disabled || loading}
+        css={{
           transition: 'all 0.2s ease',
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          backgroundColor: '$text',
-          zIndex: 1,
-        }}
-      />
-      <Box
-        sx={{
-          height: '100%',
-          width: 4,
-          transition: 'all 0.2s ease',
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          backgroundColor: '$secondary',
-          zIndex: 2,
-        }}
-      />
-      <Box
-        sx={{
-          position: 'absolute',
-          right: 0,
-          top: 0,
-          zIndex: 3,
-          width: 0,
-          height: 0,
-          borderStyle: 'solid',
-          borderWidth: '4px 4px 0 0',
-          borderColor: `${theme.palette.$text} transparent transparent transparent`,
-        }}
-      />
-      <Box
-        className="SmallButton__rotateLine"
-        sx={{
-          transition: 'all 0.2s ease',
-          position: 'absolute',
-          right: '-2px',
-          top: 2,
-          backgroundColor: '$main',
-          width: '8px',
-          height: '1px',
-          transform: 'rotate(-45deg)',
-          zIndex: 4,
-        }}
-      />
-      <Box
-        className="SmallButton__inner"
-        sx={{
-          position: 'absolute',
-          left: 0,
-          bottom: 0,
-          zIndex: 5,
-          width: '100%',
-          height: '100%',
-          cursor: 'pointer',
-          display: 'inline-flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderTop: `1px solid transparent`,
-          borderRight: `1px solid transparent`,
-          transition: 'all 0.2s ease',
-          backgroundColor: '$mainLight',
-          color: '$text',
-          p: '0px 4px 1px 5px',
+          minWidth: 95,
+          height: 20,
+          position: 'relative',
+          [theme.breakpoints.up('lg')]: {
+            minWidth: 102,
+            height: 22,
+          },
         }}>
         <Box
+          className="SmallButton__inner"
           sx={{
-            fontWeight: '400',
-            letterSpacing: '0.03em',
-            fontSize: 11,
-            lineHeight: '13px',
+            position: 'absolute',
+            left: 0,
+            bottom: 0,
+            zIndex: 5,
+            width: '100%',
+            height: '100%',
+            cursor: disabled || loading ? 'not-allowed' : 'pointer',
+            display: 'inline-flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderTop: `1px solid transparent`,
+            borderRight: `1px solid transparent`,
+            transition: 'all 0.2s ease',
+            backgroundColor: disabled || loading ? '$disabled' : '$mainLight',
+            color: '$text',
+            p: '0px 4px 1px 5px',
           }}>
-          {children}
-        </Box>
-        {loading && (
           <Box
             sx={{
-              backgroundColor: '$disabled',
-              ml: 5,
-              position: 'relative',
-              top: 0.5,
+              fontWeight: '400',
+              letterSpacing: '0.03em',
+              fontSize: 11,
+              lineHeight: '13px',
             }}>
-            <Spinner
-              size={12}
-              loaderLineColor="$paper"
-              loaderCss={{
-                background: theme.palette.$text,
-              }}
-              lineSize={2}
-            />
+            {children}
           </Box>
-        )}
-      </Box>
+          {loading && (
+            <Box
+              sx={{
+                backgroundColor: '$disabled',
+                ml: 5,
+                position: 'relative',
+                top: 0.5,
+              }}>
+              <Spinner
+                size={12}
+                loaderLineColor="$paper"
+                loaderCss={{
+                  background: theme.palette.$text,
+                }}
+                lineSize={2}
+              />
+            </Box>
+          )}
+        </Box>
+      </BoxWith3D>
     </Box>
   );
 }
