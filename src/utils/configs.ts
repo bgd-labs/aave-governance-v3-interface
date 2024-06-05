accda3e06a6fefd09876fd07fbd4a27ea28dbb7e
sakulstra <lukasstrassel@googlemail.com>
2024-06-05 12:44:26 +0200

    fix: use correct url

--- src/utils/configs.ts
+++ src/utils/configs.ts
@@ -3,11 +3,11 @@ import { initChainInformationConfig } from '@bgd-labs/frontend-web3-utils';
 import { CHAINS } from './chains';
 
 // ipfs gateway to get proposals metadata
-export const ipfsGateway = 'https://cloudflare-ipfs.com/ipfs';
+export const ipfsGateway = 'https://ipfs.io/ipfs';
 export const fallbackGateways = [
-  'https://ipfs.io',
-  'https://dweb.link',
-  'https://ipfs.runfission.com',
+  'https://dweb.link/ipfs',
+  'https://ipfs.eth.aragon.network/ipfs',
+  'https://ipfs.runfission.com/ipfs',
 ];
 
 export const chainInfoHelper = initChainInformationConfig(CHAINS);
