9e2996086d0941e18a44745fce9c5e03ff2da91c
Alex <argeares97@gmail.com>
2024-08-16 11:31:26 +0300

    fix: update rpc's

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -10,7 +10,7 @@ import { EnsDataItem } from '../web3/store/ensSlice';
 
 export enum LocalStorageKeys {
   EnsAddresses = 'EnsAddresses',
-  RpcUrls = 'rpcs_urls_2',
+  RpcUrls = 'rpcs_urls_3',
   RepresentingAddresses = 'representingAddresses',
   GaslessVote = 'isGaslessVote',
   TermsAccept = 'termsAccept',
