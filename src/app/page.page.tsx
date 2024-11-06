2d084d12086b758767e54cd9aa4346970b46b497
Alex <argeares97@gmail.com>
2024-11-06 15:15:18 +0200

    fix: deploy

--- src/app/page.page.tsx
+++ src/app/page.page.tsx
@@ -1,8 +1,10 @@
+// TODO: just for test
+
 import { Metadata } from 'next';
-import { createClient, http } from 'viem';
 import { mainnet } from 'viem/chains';
 
 import { metaTexts } from '../helpers/texts/metaTexts';
+import { createViemClient } from '../old/utils/chains';
 import { api } from '../trpc/server';
 
 export const metadata: Metadata = {
@@ -15,21 +17,10 @@ export const metadata: Metadata = {
   },
 };
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  // const activePage = searchParams['activePage']
-  //   ? Number(searchParams['activePage']) - 1
-  //   : 0;
-
-  const client = createClient({
-    chain: mainnet,
-    transport: http(),
+export default async function Page() {
+  const data = await api.configs.get({
+    govCoreClient: createViemClient(mainnet, ''),
   });
-
-  const data = await api.configs.get({ govCoreClient: client });
 
   return <h1>Hello world {data.totalProposalsCount}</h1>;
 }
