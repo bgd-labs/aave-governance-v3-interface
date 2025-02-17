77ed90b51e6351fd3fdff9dbd779ebcbacc4d1bb
K.K. <m@mnm.mn>
2025-02-17 16:18:40 +0000

    fix: revert config update

--- next.config.js
+++ next.config.js
@@ -14,6 +14,18 @@ const nextConfig = {
         source: '/manifest.json',
         headers: [
           {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, content-type, Authorization',
+          },
+          {
             key: 'Content-Security-Policy',
             value: 'frame-ancestors "self" https://app.safe.global;',
           },
