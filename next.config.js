00e6536a9e18c62b7dd3c079bf468ea108849021
K.K. <m@mnm.mn>
2025-02-17 16:13:42 +0000

    chore: update nextjs config

--- next.config.js
+++ next.config.js
@@ -11,22 +11,10 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/manifest.json",
+        source: '/manifest.json',
         headers: [
           {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-Requested-With, content-type, Authorization",
-          },
-          {
-            key: "Content-Security-Policy",
+            key: 'Content-Security-Policy',
             value: 'frame-ancestors "self" https://app.safe.global;',
           },
         ],
