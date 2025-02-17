6f5a0f37d6e38949111ca2b392f97cc119087b12
K.K. <m@mnm.mn>
2025-02-17 15:53:40 +0000

    feat: auto connect safe wallet

--- next.config.js
+++ next.config.js
@@ -8,6 +8,31 @@ const nextConfig = {
   experimental: {
     webpackBuildWorker: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/manifest.json",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, content-type, Authorization",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: 'frame-ancestors "self" https://app.safe.global;',
+          },
+        ],
+      },
+    ];
+  },
   webpack(config) {
     // config.resolve.fallback = { fs: false, path: false };
 
